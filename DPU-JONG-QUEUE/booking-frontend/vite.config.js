5bf07574f3064b38b36d55a75a587ab697d6e666
NATTO <donut.dpu2025@gmail.com>
2026-05-22 00:43:46 +0700

    d

--- DPU-JONG-QUEUE/booking-frontend/vite.config.js
+++ DPU-JONG-QUEUE/booking-frontend/vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
@@ -7,9 +6,11 @@ import path from 'path'
 export default defineConfig({
   plugins: [
     vue(),
-    vuetify({ autoImport: true }), // <<< สำคัญ
+    vuetify({ autoImport: true }),
   ],
   resolve: {
-    alias: { '@': path.resolve(__dirname, './src') },
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
